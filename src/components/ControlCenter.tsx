37baf11ea16919f2c1ee147066434942494be65e
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-01-12 23:04:21 +0600

    track icon added

--- src/components/ControlCenter.tsx
+++ src/components/ControlCenter.tsx
@@ -1,31 +1,50 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Pressable, StyleSheet, View} from 'react-native';
 import TrackPlayer, {State, usePlaybackState} from 'react-native-track-player';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {playbackService} from '../../musicPlayerServices';
 
 const ControlCenter = () => {
   const playBackState = usePlaybackState();
-
+  // next button
   const skipToNext = async () => {
     await TrackPlayer.skipToNext();
   };
-
+  // Previous button
   const skipToPrevious = async () => {
     await TrackPlayer.skipToPrevious();
   };
 
   const togglePlayback = async (playback: State) => {
     const currentTrack = await TrackPlayer.getCurrentTrack();
-    if (playback === State.Paused || playback === State.Ready) {
-      await TrackPlayer.play();
-    } else {
-      await TrackPlayer.pause();
+
+    if (currentTrack !== null) {
+      if (playback === State.Paused || playback === State.Ready) {
+        await TrackPlayer.play();
+      } else {
+        await TrackPlayer.pause();
+      }
     }
   };
 
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={skipToPrevious}>
+        <Icon style={styles.icon} name="skip-previous" size={40} />
+      </Pressable>
+      <Pressable onPress={() => togglePlayback(playBackState)}>
+        <Icon
+          style={styles.icon}
+          name={playBackState === State.Playing ? 'pause' : 'play-arrow'}
+          size={75}
+        />
+      </Pressable>
+      <Pressable onPress={skipToNext}>
+        <Icon style={styles.icon} name="skip-next" size={40} />
+      </Pressable>
+    </View>
+  );
 };
+
 const styles = StyleSheet.create({
   container: {
     marginBottom: 56,
@@ -42,3 +61,5 @@ const styles = StyleSheet.create({
   },
 });
 export default ControlCenter;
+
+// 1337x.to

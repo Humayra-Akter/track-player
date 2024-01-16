3db8445bb9c4af3dfc5bef182cec4a4a83496e86
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-01-16 22:33:00 +0600

    app edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import type {PropsWithChildren} from 'react';
-
+import React, {useState, useEffect} from 'react';
 import {
   ActivityIndicator,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
 import {setupPlayer, addTrack} from '../musicPlayerServices';
+import MusicPlayer from './screens/MusicPlayer';
 
-function App(): React.JSX.Element {
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
+function App(): JSX.Element {
+  const [isPlayerReady, setIsPaylerReady] = useState(false);
 
   async function setup() {
     let isSetup = await setupPlayer();
+
     if (isSetup) {
       await addTrack();
     }
 
-    setIsPlayerReady(isSetup);
+    setIsPaylerReady(isSetup);
   }
 
   useEffect(() => {
@@ -39,12 +35,10 @@ function App(): React.JSX.Element {
   }
 
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <View>
-        <Text>hiiii</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'} />
+      <MusicPlayer />
+    </View>
   );
 }
 
